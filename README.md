96632e59058bbc370ede26a1dfecfab1a52cfcf8
Ahmed Nouira <ahmnouira@gmail.com>
2020-01-05 23:54:06 +0100

    add slides

---
+++ README.md
@@ -0,0 +1,7 @@
+# saving-memories
+
+Ionic 4 application allows you to capture pictures using the native camera of the mobile device.
+
+## Screenshots 
+
+![img](/img/img.png)
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx"; 
+
+const camera : Camera = new Camera();
+
+const options : CameraOptions  = {
+
+  quality: 100,
+  correctOrientation: true,
+  saveToPhotoAlbum: true,
+  destinationType: camera.DestinationType.DATA_URL,
+  encodingType: camera.EncodingType.JPEG,
+  cameraDirection: camera.Direction.FRONT,
+  sourceType: camera.PictureSourceType.CAMERA,
+  mediaType: camera.MediaType.ALLMEDIA,
+  targetHeight: 800,
+  targetWidth: 480
+} 
 
 @Component({
   selector: 'app-home',
@@ -7,6 +24,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  images : string [] = new Array();
+  currentImage : string;
+
+  // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  
+  constructor(private camera : Camera) {}
+
+  takePicture() : void {
+    this.camera.getPicture(options).then((imageData : string) => {
+      console.log('imageData:', imageData);
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+      this.images.push(this.currentImage);
+      let imagesUris = this.images.join(','); 
+      console.log('imagesUris: ', imagesUris);
+    }).catch((err) => console.log(err));
+
+  }  
 
 }
